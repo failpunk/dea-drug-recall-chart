a8d2c426677a05c655923fdc0a2787e6b829fc34
Justin <failpunk@gmail.com>
2019-05-21 17:50:35 -0700

    Ensure we are pulling all reports because API is paginated.

--- chart-service.js
+++ chart-service.js
@@ -1,16 +1,39 @@
 export const chartService = () => {
     let currentChart;
+    let apiResults = [];
+    let resultSet = [];
 
-    // TODO: NEED TO MAKE MULTIPLE CALLS TO GET ALL DATA.
     // TODO: DATA SHOULD BE LAST 12 MONTHS.
 
     async function fetchData(state) {
-        let url = `https://api.fda.gov/drug/enforcement.json?search=report_date:[20180101+TO+20191231]+AND+state:${state}&limit=100`;
+        let data = [];
+        let skip = 0;
+        let total = Number.POSITIVE_INFINITY;
+
+        // keep callingn API until results run out.
+        while (skip <= total) {
+            let { meta, results } = await fetchResultBatch(state, skip);
+
+            // recalc the current page number.
+            skip += meta.results.limit;
+            total = meta.results.total;
+
+            // add batch to the total data.
+            data = data.concat(results);
+        }
+
+        return data;
+    }
+
+    // Fetches a single batch of results from the API (maximum 100)
+    async function fetchResultBatch(state, skip, limit = 100) {
+        let url = `https://api.fda.gov/drug/enforcement.json?search=report_date:[20180101+TO+20191231]+AND+state:${state}&limit=${limit}&skip=${skip}`;
 
         let response = await window.fetch(url);
         return response.json();
     }
 
+    // Aggregate by month and format raw API data to be used with our charting software.
     function formatData(data) {
         let monthData = {};
 
@@ -27,6 +50,7 @@ export const chartService = () => {
         return Object.keys(monthData).map(key => monthData[key]);
     }
 
+    // Instantiates a new chart with our updated dataset.
     function drawChart(data, labels) {
         currentChart = new Chartist.Bar(
             '.chart',
@@ -40,18 +64,30 @@ export const chartService = () => {
         );
     }
 
+    /**
+     * Public methods
+     */
     return {
+        // Render a chart for a given state code.
         async render(state) {
             try {
-                let data = await fetchData(state);
-                let formattedData = formatData(data.results);
+                apiResults = await fetchData(state);
+                resultSet = formatData(apiResults);
 
-                drawChart(formattedData, this.chartLabels);
+                drawChart(resultSet, this.chartLabels);
 
                 return true;
             } catch (error) {
-                console.log('ERROR Loading Data!', data);
+                console.log('ERROR Loading Data!', error);
             }
+        },
+
+        get resultSet() {
+            return resultSet;
+        },
+
+        get apiResults() {
+            return apiResults;
         },
 
         get chartLabels() {
